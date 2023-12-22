4465d1c1d21aeac8a4c6e83ba20a3101962cdcdc
Rohan <walkerrandy930@gmail.com>
2023-12-22 08:07:20 +0545

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  const[search, setSearch] = useState('');
+
+  const[inStockOnly, setinStockOnly] = useState(false);
+
+  const handleChange = (e) =>{
+    setSearch (e.target.value);
+  };
+
+  const toggleinStock = (e)=> {
+    setinStockOnly(e.target.checked);
+  }
+  return(
+    <div className='app'>
+     <input onChange={handleChange} value={search} placeholder='Search....'/>
+     <div>
+     <input type='checkbox' onChange={handleChange} checked={inStockOnly} id='checkbox'/>
+     <label htmlFor='checkbox'> ABC Show only products in stock</label>
+     </div>
     </div>
-  );
-}
-
+  )
+};
 export default App;
