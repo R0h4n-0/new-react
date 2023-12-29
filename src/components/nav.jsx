da96476c2cae3a8112c0bd2614eb51f54e2af99b
Rohan <walkerrandy930@gmail.com>
2023-12-29 08:30:20 +0545

    27th dec

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+// import {Nav} from'./components/Nav';
 
 export const Nav = () => {
     return (
         <header>
             <nav>
-                <Link to = {'/'}> Home </Link>
-                <Link to = {'/products'}>products</Link>
+                <Link to = {'/'} className='mx-4'> Home </Link>
+                <Link to = {'/products'} className='mx-4'>products</Link>
             </nav>
         </header>
     );
